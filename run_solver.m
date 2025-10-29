da1c58fbea6536b4ac6b72dda0781b799d2599e6
Arya Gadage <aryaggadage@gmail.com>
2025-10-29 10:39:04 -0400

    Extract 'Observed_Prob' from p_obs if table
    Added extraction of the 'Observed_Prob' column from p_obs if it is a table.

--- run_solver.m
+++ run_solver.m
@@ -1,4 +1,11 @@
 load("/Users/ag5276/Documents/Haoge/optimization stuff/p_obs.mat")   
 n = 5;
-
+%% Extract the numeric probability column
+if istable(p_obs)
+    if any(strcmp('Observed_Prob', p_obs.Properties.VariableNames))
+        p_obs = p_obs.Observed_Prob;
+    else
+        error('Expected column "Observed_Prob" not found in p_obs table.');
+    end
+end
 [lambda_opt, V, rankings, choice_sets, error_val] = solve_rum_projection(p_obs, n);
